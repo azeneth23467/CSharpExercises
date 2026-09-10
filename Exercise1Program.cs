b659137f43c27eed8bb438cd29c015bde4e5830b
azeneth23467 <agallegosperez@byupathway.edu>
2026-09-09 20:56:15 -0600

    Added Exercise 1

---
+++ Exercise1Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ask for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display the result
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
+}
